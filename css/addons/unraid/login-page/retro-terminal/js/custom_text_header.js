289354440ec92d511be385fb0c4d1d40639e370c
LaFours23 <mtrainor@gmail.com>
2024-01-03 15:24:15 -0500

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -7,12 +7,13 @@
 
 let custom_text_header = `
 <div class="custom-text-header"> <pre> 
+
 ___  ________ _____   _____                          
 |  \/  |_   _|____ | /  ___|                         
 | .  . | | |     / / \ `--.  ___ _ ____   _____ _ __ 
 | |\/| | | |     \ \  `--. \/ _ \ '__\ \ / / _ \ '__|
 | |  | | | | .___/ / /\__/ /  __/ |   \ V /  __/ |   
 \_|  |_/ \_/ \____/  \____/ \___|_|    \_/ \___|_|   
-                                                                 </pre> </div>
+                                                    </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
